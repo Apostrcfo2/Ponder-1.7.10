5c38d0d541494e4ac74c133298320fd865059c1c
Apostrcfo2 <temp24919@gmail.com>
2026-04-30 18:04:14 -0300

    Port StoryBoardEntry and PonderPlugin - replace record with class, adapt ResourceLocation package

--- src/main/java/net/createmod/ponder1710/api/registration/PonderPlugin.java
+++ src/main/java/net/createmod/ponder1710/api/registration/PonderPlugin.java
@@ -1,34 +1,26 @@
 package net.createmod.ponder1710.api.registration;
 
 import net.createmod.ponder1710.api.level.PonderLevel;
-import net.minecraft.resources.ResourceLocation;
+
+// import net.minecraft.resources.ResourceLocation; // 1.7.10 uses different package
+import net.minecraft.util.ResourceLocation;
 
 public interface PonderPlugin {
 
-	/**
-	 * @return the modID of the mod that added this plugin
-	 */
-	String getModId();
+    String getModId();
 
-	/**
-	 * Register all the Ponder Scenes added by your Mod
-	 */
-	default void registerScenes(PonderSceneRegistrationHelper<ResourceLocation> helper) {
-	}
+    default void registerScenes(PonderSceneRegistrationHelper<ResourceLocation> helper) {
+    }
 
-	/**
-	 * Register all the Ponder Tags added by your Mod
-	 */
-	default void registerTags(PonderTagRegistrationHelper<ResourceLocation> helper) {
-	}
+    default void registerTags(PonderTagRegistrationHelper<ResourceLocation> helper) {
+    }
 
-	default void registerSharedText(SharedTextRegistrationHelper helper) {
-	}
+    default void registerSharedText(SharedTextRegistrationHelper helper) {
+    }
 
-	default void onPonderLevelRestore(PonderLevel ponderLevel) {
-	}
+    default void onPonderLevelRestore(PonderLevel ponderLevel) {
+    }
 
-	default void indexExclusions(IndexExclusionHelper helper) {
-	}
-
+    default void indexExclusions(IndexExclusionHelper helper) {
+    }
 }
--- src/main/java/net/createmod/ponder1710/api/registration/StoryBoardEntry.java
+++ src/main/java/net/createmod/ponder1710/api/registration/StoryBoardEntry.java
@@ -3,100 +3,73 @@ package net.createmod.ponder1710.api.registration;
 import java.util.List;
 
 import net.createmod.ponder1710.api.scene.PonderStoryBoard;
-import net.minecraft.resources.ResourceLocation;
+
+// import net.minecraft.resources.ResourceLocation; // 1.7.10 uses different package
+import net.minecraft.util.ResourceLocation;
 
 public interface StoryBoardEntry {
-	PonderStoryBoard getBoard();
 
-	String getNamespace();
+    PonderStoryBoard getBoard();
 
-	ResourceLocation getSchematicLocation();
+    String getNamespace();
 
-	ResourceLocation getComponent();
+    ResourceLocation getSchematicLocation();
 
-	List<ResourceLocation> getTags();
+    ResourceLocation getComponent();
 
-	List<SceneOrderingEntry> getOrderingEntries();
+    List<ResourceLocation> getTags();
 
-	/**
-	 * inside the PonderUI, will order this scene somewhere <i>before</i> the other scene.
-	 * only has an effect if a scene with the given id can be found.
-	 * <br />
-	 * use {@link StoryBoardEntry#orderBefore(String, String)} to target scenes added by other mods.
-	 *
-	 * @param otherSceneId id of the scene that should appear after this one
-	 * @return this StoryBoardEntry
-	 */
-	default StoryBoardEntry orderBefore(String otherSceneId) {
-		return orderBefore(getNamespace(), otherSceneId);
-	}
+    List<SceneOrderingEntry> getOrderingEntries();
 
-	/**
-	 * inside the PonderUI, will order this scene somewhere <i>before</i> the other scene.
-	 * only has an effect if a scene with the given id can be found.
-	 *
-	 * @param namespace    modId of the mod that added the other scene
-	 * @param otherSceneId id of the scene that should appear after this one
-	 * @return this StoryBoardEntry
-	 */
-	StoryBoardEntry orderBefore(String namespace, String otherSceneId);
+    default StoryBoardEntry orderBefore(String otherSceneId) {
+        return orderBefore(getNamespace(), otherSceneId);
+    }
 
-	/**
-	 * inside the PonderUI, will order this scene somewhere <i>after</i> the other scene.
-	 * only has an effect if a scene with the given id can be found.
-	 * <br />
-	 * use {@link StoryBoardEntry#orderAfter(String, String)} to target scenes added by other mods.
-	 *
-	 * @param otherSceneId id of the scene that should appear before this one
-	 * @return this StoryBoardEntry
-	 */
-	default StoryBoardEntry orderAfter(String otherSceneId) {
-		return orderAfter(getNamespace(), otherSceneId);
-	}
+    StoryBoardEntry orderBefore(String namespace, String otherSceneId);
 
-	/**
-	 * inside the PonderUI, will order this scene somewhere <i>after</i> the other scene.
-	 * only has an effect if a scene with the given id can be found.
-	 *
-	 * @param namespace    modId of the mod that added the other scene
-	 * @param otherSceneId id of the scene that should appear before this one
-	 * @return this StoryBoardEntry
-	 */
-	StoryBoardEntry orderAfter(String namespace, String otherSceneId);
+    default StoryBoardEntry orderAfter(String otherSceneId) {
+        return orderAfter(getNamespace(), otherSceneId);
+    }
 
-	/**
-	 * causes the supplied PonderTag to flash when viewing this scene in the PonderUI
-	 *
-	 * @return this StoryBoardEntry
-	 */
-	StoryBoardEntry highlightTag(ResourceLocation tag);
+    StoryBoardEntry orderAfter(String namespace, String otherSceneId);
 
-	/**
-	 * causes the supplied PonderTags to flash when viewing this scene in the PonderUI
-	 *
-	 * @return this StoryBoardEntry
-	 */
-	StoryBoardEntry highlightTags(ResourceLocation... tags);
+    StoryBoardEntry highlightTag(ResourceLocation tag);
 
-	/**
-	 * causes all assigned PonderTags to flash when viewing this scene in the PonderUI
-	 *
-	 * @return this StoryBoardEntry
-	 */
-	StoryBoardEntry highlightAllTags();
+    StoryBoardEntry highlightTags(ResourceLocation... tags);
 
-	enum SceneOrderingType {
-		BEFORE, AFTER
-	}
+    StoryBoardEntry highlightAllTags();
 
-	record SceneOrderingEntry(SceneOrderingType type, ResourceLocation sceneId) {
+    enum SceneOrderingType {
+        BEFORE, AFTER
+    }
 
-		public static SceneOrderingEntry after(String namespace, String sceneId) {
-			return new SceneOrderingEntry(SceneOrderingType.AFTER, ResourceLocation.fromNamespaceAndPath(namespace, sceneId));
-		}
+    // TODO: record not available in Java 8 - replaced with class
+    // record SceneOrderingEntry(SceneOrderingType type, ResourceLocation sceneId) { ... }
+    class SceneOrderingEntry {
 
-		public static SceneOrderingEntry before(String namespace, String sceneId) {
-			return new SceneOrderingEntry(SceneOrderingType.BEFORE, ResourceLocation.fromNamespaceAndPath(namespace, sceneId));
-		}
-	}
+        private final SceneOrderingType type;
+        private final ResourceLocation sceneId;
+
+        public SceneOrderingEntry(SceneOrderingType type, ResourceLocation sceneId) {
+            this.type = type;
+            this.sceneId = sceneId;
+        }
+
+        public SceneOrderingType type() {
+            return type;
+        }
+
+        public ResourceLocation sceneId() {
+            return sceneId;
+        }
+
+        public static SceneOrderingEntry after(String namespace, String sceneId) {
+            // ResourceLocation.fromNamespaceAndPath not available in 1.7.10
+            return new SceneOrderingEntry(SceneOrderingType.AFTER, new ResourceLocation(namespace, sceneId));
+        }
+
+        public static SceneOrderingEntry before(String namespace, String sceneId) {
+            return new SceneOrderingEntry(SceneOrderingType.BEFORE, new ResourceLocation(namespace, sceneId));
+        }
+    }
 }
