08b32023ad3cebf54b4c880e42876cd07dcdc885
Apostrcfo2 <temp24919@gmail.com>
2026-04-29 22:22:56 -0300

    some adjust

--- src/main/java/net/createmod/ponder1710/foundation/instruction/HideAllInstruction.java
+++ src/main/java/net/createmod/ponder1710/foundation/instruction/HideAllInstruction.java
@@ -1,54 +1,53 @@
 package net.createmod.ponder1710.foundation.instruction;
 
-import net.createmod.ponder1710.api.element.AnimatedOverlayElement;
-import net.createmod.ponder1710.api.element.AnimatedSceneElement;
 import net.createmod.ponder1710.foundation.PonderScene;
 import net.createmod.ponder1710.foundation.element.AnimatedOverlayElementBase;
 import net.createmod.ponder1710.foundation.element.AnimatedSceneElementBase;
-import net.minecraft.core.Direction;
-import net.minecraft.world.phys.Vec3;
 
+// TODO: Direction and Vec3 fadeOutTo not available in 1.7.10 form yet
+// Original used net.minecraft.core.Direction and net.minecraft.world.phys.Vec3
 public class HideAllInstruction extends TickingInstruction {
 
-	private final Direction fadeOutTo;
+    // private final Direction fadeOutTo;
+    // Replaced with null for now - fade direction not yet implemented
 
-	public HideAllInstruction(int fadeOutTicks, Direction fadeOutTo) {
-		super(false, fadeOutTicks);
-		this.fadeOutTo = fadeOutTo;
-	}
+    public HideAllInstruction(int fadeOutTicks, /* Direction fadeOutTo */ Object fadeOutTo) {
+        super(false, fadeOutTicks);
+        // this.fadeOutTo = fadeOutTo;
+    }
 
-	@Override
-	protected void firstTick(PonderScene scene) {
-		super.firstTick(scene);
-		scene.getElements()
-			.forEach(element -> {
-				if (element instanceof AnimatedSceneElementBase animatedSceneElement) {
-					animatedSceneElement.setFade(1);
-					animatedSceneElement
-						.setFadeVec(fadeOutTo == null ? null : Vec3.atLowerCornerOf(fadeOutTo.getNormal()).scale(.5f));
-				} else if (element instanceof AnimatedOverlayElementBase animatedSceneElement) {
-					animatedSceneElement.setFade(1);
-				} else
-					element.setVisible(false);
-			});
-	}
+    @Override
+    protected void firstTick(PonderScene scene) {
+        super.firstTick(scene);
+        scene.getElements()
+            .forEach(element -> {
+                if (element instanceof AnimatedSceneElementBase animatedSceneElement) {
+                    animatedSceneElement.setFade(1);
+                    // TODO: setFadeVec not yet implemented - requires Direction/Vec3 port
+                    // animatedSceneElement.setFadeVec(fadeOutTo == null ? null : Vec3.atLowerCornerOf(fadeOutTo.getNormal()).scale(.5f));
+                } else if (element instanceof AnimatedOverlayElementBase animatedSceneElement) {
+                    animatedSceneElement.setFade(1);
+                } else {
+                    element.setVisible(false);
+                }
+            });
+    }
 
-	@Override
-	public void tick(PonderScene scene) {
-		super.tick(scene);
-		float fade = (remainingTicks / (float) totalTicks);
+    @Override
+    public void tick(PonderScene scene) {
+        super.tick(scene);
+        float fade = (remainingTicks / (float) totalTicks);
 
-		scene.forEach(AnimatedSceneElementBase.class, ase -> {
-			ase.setFade(fade * fade);
-			if (remainingTicks == 0)
-				ase.setFade(0);
-		});
+        scene.forEach(AnimatedSceneElementBase.class, ase -> {
+            ase.setFade(fade * fade);
+            if (remainingTicks == 0)
+                ase.setFade(0);
+        });
 
-		scene.forEach(AnimatedOverlayElementBase.class, aoe -> {
-			aoe.setFade(fade * fade);
-			if (remainingTicks == 0)
-				aoe.setFade(0);
-		});
-	}
-
+        scene.forEach(AnimatedOverlayElementBase.class, aoe -> {
+            aoe.setFade(fade * fade);
+            if (remainingTicks == 0)
+                aoe.setFade(0);
+        });
+    }
 }
--- src/main/java/net/createmod/ponder1710/foundation/instruction/RotateSceneInstruction.java
+++ src/main/java/net/createmod/ponder1710/foundation/instruction/RotateSceneInstruction.java
@@ -1,33 +1,36 @@
 package net.createmod.ponder1710.foundation.instruction;
 
-import net.createmod.catnip.animation.LerpedFloat.Chaser;
 import net.createmod.ponder1710.foundation.PonderScene;
 import net.createmod.ponder1710.foundation.PonderScene.SceneTransform;
 
+// TODO: LerpedFloat.Chaser from catnip not available in 1.7.10
+// Replaced with direct value setting until LerpedFloat is ported
 public class RotateSceneInstruction extends PonderInstruction {
 
-	private final float xRot;
-	private final float yRot;
-	private final boolean relative;
+    private final float xRot;
+    private final float yRot;
+    private final boolean relative;
 
-	public RotateSceneInstruction(float xRot, float yRot, boolean relative) {
-		this.xRot = xRot;
-		this.yRot = yRot;
-		this.relative = relative;
-	}
+    public RotateSceneInstruction(float xRot, float yRot, boolean relative) {
+        this.xRot = xRot;
+        this.yRot = yRot;
+        this.relative = relative;
+    }
 
-	@Override
-	public boolean isComplete() {
-		return true;
-	}
+    @Override
+    public boolean isComplete() {
+        return true;
+    }
 
-	@Override
-	public void tick(PonderScene scene) {
-		SceneTransform transform = scene.getTransform();
-		float targetX = relative ? transform.xRotation.getChaseTarget() + xRot : xRot;
-		float targetY = relative ? transform.yRotation.getChaseTarget() + yRot : yRot;
-		transform.xRotation.chase(targetX, .1f, Chaser.EXP);
-		transform.yRotation.chase(targetY, .1f, Chaser.EXP);
-	}
-
+    @Override
+    public void tick(PonderScene scene) {
+        SceneTransform transform = scene.getTransform();
+        float targetX = relative ? transform.xRotation + xRot : xRot;
+        float targetY = relative ? transform.yRotation + yRot : yRot;
+        // TODO: Use smooth chasing once LerpedFloat is ported from catnip
+        // transform.xRotation.chase(targetX, .1f, Chaser.EXP);
+        // transform.yRotation.chase(targetY, .1f, Chaser.EXP);
+        transform.xRotation = targetX;
+        transform.yRotation = targetY;
+    }
 }
