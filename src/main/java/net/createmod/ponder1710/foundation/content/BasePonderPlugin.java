df8762f1010bc91b82f61e3459d66008e3e1d557
Apostrcfo2 <temp24919@gmail.com>
2026-05-02 15:45:09 -0300

    Port foundation/content files - comment modern APIs, adapt to 1.7.10 equivalents

--- src/main/java/net/createmod/ponder1710/foundation/content/BasePonderPlugin.java
+++ src/main/java/net/createmod/ponder1710/foundation/content/BasePonderPlugin.java
@@ -6,14 +6,14 @@ import net.createmod.ponder1710.api.registration.SharedTextRegistrationHelper;
 
 public class BasePonderPlugin implements PonderPlugin {
 
-	@Override
-	public String getModId() {
-		return Ponder.MOD_ID;
-	}
+    @Override
+    public String getModId() {
+        return Ponder.MOD_ID;
+    }
 
-	@Override
-	public void registerSharedText(SharedTextRegistrationHelper helper) {
-		helper.registerSharedText("sneak_and", "Sneak +");
-		helper.registerSharedText("ctrl_and", "Ctrl +");
-	}
+    @Override
+    public void registerSharedText(SharedTextRegistrationHelper helper) {
+        helper.registerSharedText("sneak_and", "Sneak +");
+        helper.registerSharedText("ctrl_and", "Ctrl +");
+    }
 }
--- src/main/java/net/createmod/ponder1710/foundation/content/DebugPonderPlugin.java
+++ src/main/java/net/createmod/ponder1710/foundation/content/DebugPonderPlugin.java
@@ -3,17 +3,19 @@ package net.createmod.ponder1710.foundation.content;
 import net.createmod.ponder1710.Ponder;
 import net.createmod.ponder1710.api.registration.PonderPlugin;
 import net.createmod.ponder1710.api.registration.PonderSceneRegistrationHelper;
-import net.minecraft.resources.ResourceLocation;
+
+// import net.minecraft.resources.ResourceLocation; // different package in 1.7.10
+import net.minecraft.util.ResourceLocation;
 
 public class DebugPonderPlugin implements PonderPlugin {
 
-	@Override
-	public String getModId() {
-		return Ponder.MOD_ID;
-	}
+    @Override
+    public String getModId() {
+        return Ponder.MOD_ID;
+    }
 
-	@Override
-	public void registerScenes(PonderSceneRegistrationHelper<ResourceLocation> helper) {
-		DebugScenes.registerAll(helper);
-	}
+    @Override
+    public void registerScenes(PonderSceneRegistrationHelper<ResourceLocation> helper) {
+        DebugScenes.registerAll(helper);
+    }
 }
