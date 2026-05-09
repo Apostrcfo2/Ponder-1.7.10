c599d66e864126485704913f6173d5955afeb691
Apostrcfo2 <temp24919@gmail.com>
2026-05-08 22:51:24 -0300

    Port PonderLocalization and DefaultPonderSceneRegistrationHelper - adapt I18n/ResourceLocation/Couple to 1.7.10

--- src/main/java/net/createmod/ponder1710/foundation/registration/DefaultPonderSceneRegistrationHelper.java
+++ src/main/java/net/createmod/ponder1710/foundation/registration/DefaultPonderSceneRegistrationHelper.java
@@ -8,56 +8,58 @@ import net.createmod.ponder1710.api.registration.PonderSceneRegistrationHelper;
 import net.createmod.ponder1710.api.registration.StoryBoardEntry;
 import net.createmod.ponder1710.api.scene.PonderStoryBoard;
 import net.createmod.ponder1710.foundation.PonderStoryBoardEntry;
-import net.minecraft.resources.ResourceLocation;
+
+// import net.minecraft.resources.ResourceLocation; // different package in 1.7.10
+import net.minecraft.util.ResourceLocation;
 
 public class DefaultPonderSceneRegistrationHelper implements PonderSceneRegistrationHelper<ResourceLocation> {
 
-	protected String namespace;
-	protected PonderSceneRegistry sceneRegistry;
+    protected String namespace;
+    protected PonderSceneRegistry sceneRegistry;
 
-	public DefaultPonderSceneRegistrationHelper(String namespace, PonderSceneRegistry sceneRegistry) {
-		this.namespace = namespace;
-		this.sceneRegistry = sceneRegistry;
-	}
+    public DefaultPonderSceneRegistrationHelper(String namespace, PonderSceneRegistry sceneRegistry) {
+        this.namespace = namespace;
+        this.sceneRegistry = sceneRegistry;
+    }
 
-	@Override
-	public <T> GenericPonderSceneRegistrationHelper<T> withKeyFunction(Function<T, ResourceLocation> keyGen) {
-		return new GenericPonderSceneRegistrationHelper<>(this, keyGen);
-	}
+    @Override
+    public <T> GenericPonderSceneRegistrationHelper<T> withKeyFunction(Function<T, ResourceLocation> keyGen) {
+        return new GenericPonderSceneRegistrationHelper<>(this, keyGen);
+    }
 
-	@Override
-	public StoryBoardEntry addStoryBoard(ResourceLocation component, ResourceLocation schematicLocation,
-										 PonderStoryBoard storyBoard, ResourceLocation... tags) {
-		StoryBoardEntry entry = this.createStoryBoardEntry(storyBoard, schematicLocation, component);
-		entry.highlightTags(tags);
-		sceneRegistry.addStoryBoard(entry);
-		return entry;
-	}
+    @Override
+    public StoryBoardEntry addStoryBoard(ResourceLocation component, ResourceLocation schematicLocation,
+        PonderStoryBoard storyBoard, ResourceLocation... tags) {
+        StoryBoardEntry entry = createStoryBoardEntry(storyBoard, schematicLocation, component);
+        entry.highlightTags(tags);
+        sceneRegistry.addStoryBoard(entry);
+        return entry;
+    }
 
-	@Override
-	public StoryBoardEntry addStoryBoard(ResourceLocation component, String schematicPath,
-										 PonderStoryBoard storyBoard, ResourceLocation... tags) {
-		return addStoryBoard(component, asLocation(schematicPath), storyBoard, tags);
-	}
+    @Override
+    public StoryBoardEntry addStoryBoard(ResourceLocation component, String schematicPath,
+        PonderStoryBoard storyBoard, ResourceLocation... tags) {
+        return addStoryBoard(component, asLocation(schematicPath), storyBoard, tags);
+    }
 
-	@Override
-	public MultiSceneBuilder forComponents(ResourceLocation... components) {
-		return new GenericMultiSceneBuilder<>(this, Arrays.asList(components));
-	}
+    @Override
+    public MultiSceneBuilder forComponents(ResourceLocation... components) {
+        return new GenericMultiSceneBuilder<>(this, Arrays.asList(components));
+    }
 
-	@Override
-	public MultiSceneBuilder forComponents(Iterable<? extends ResourceLocation> components) {
-		return new GenericMultiSceneBuilder<>(this, components);
-	}
+    @Override
+    public MultiSceneBuilder forComponents(Iterable<? extends ResourceLocation> components) {
+        return new GenericMultiSceneBuilder<>(this, components);
+    }
 
-	@Override
-	public ResourceLocation asLocation(String path) {
-		return ResourceLocation.fromNamespaceAndPath(namespace, path);
-	}
+    @Override
+    public ResourceLocation asLocation(String path) {
+        // ResourceLocation.fromNamespaceAndPath not available in 1.7.10
+        return new ResourceLocation(namespace, path);
+    }
 
-	private PonderStoryBoardEntry createStoryBoardEntry(PonderStoryBoard storyBoard, ResourceLocation schematicLocation,
-														ResourceLocation component) {
-		return new PonderStoryBoardEntry(storyBoard, namespace, schematicLocation, component);
-	}
-
+    private PonderStoryBoardEntry createStoryBoardEntry(PonderStoryBoard storyBoard, ResourceLocation schematicLocation,
+        ResourceLocation component) {
+        return new PonderStoryBoardEntry(storyBoard, namespace, schematicLocation, component);
+    }
 }
