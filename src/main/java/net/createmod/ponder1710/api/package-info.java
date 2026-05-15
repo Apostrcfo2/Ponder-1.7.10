142803bf88a883a4a9ffb25244dd4b78cbaf3f68
Apostrcfo2 <temp24919@gmail.com>
2026-05-15 17:57:49 -0300

    Fix package-info.java files - comment unavailable 1.7.10 annotations

--- src/main/java/net/createmod/ponder1710/api/package-info.java
+++ src/main/java/net/createmod/ponder1710/api/package-info.java
@@ -4,5 +4,5 @@ package net.createmod.ponder1710.api;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 // TODO: MethodsReturnNonnullByDefault and FieldsAreNonnullByDefault not available in 1.7.10
-// import net.minecraft.FieldsAreNonnullByDefault;
-// import net.minecraft.MethodsReturnNonnullByDefault;
+// // import net.minecraft.FieldsAreNonnullByDefault;
+// // import net.minecraft.MethodsReturnNonnullByDefault;
--- src/main/java/net/createmod/ponder1710/api/registration/package-info.java
+++ src/main/java/net/createmod/ponder1710/api/registration/package-info.java
@@ -1,9 +1,9 @@
 @ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@FieldsAreNonnullByDefault
+// @MethodsReturnNonnullByDefault // not available in 1.7.10
+// @FieldsAreNonnullByDefault // not available in 1.7.10
 package net.createmod.ponder1710.api.registration;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
-import net.minecraft.FieldsAreNonnullByDefault;
-import net.minecraft.MethodsReturnNonnullByDefault;
+// import net.minecraft.FieldsAreNonnullByDefault;
+// import net.minecraft.MethodsReturnNonnullByDefault;
