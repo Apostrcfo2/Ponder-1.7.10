87c64bca522d7172987fb8f68b58ad1859ea291a
Apostrcfo2 <temp24919@gmail.com>
2026-05-15 23:26:48 -0300

    metanip: port data/Pair, data/Couple, data/Iterate to 1.7.10

--- src/main/java/net/createmod/metanip/data/Iterate.java
+++ src/main/java/net/createmod/metanip/data/Iterate.java
@@ -1,58 +1,51 @@
 package net.createmod.metanip.data;
 
 import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.List;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.core.Direction.Axis;
+// import net.minecraft.core.BlockPos; // 1.7.10 uses x,y,z
+// import net.minecraft.core.Direction; // ForgeDirection in 1.7.10
+// import net.minecraft.core.Direction.Axis; // not available in 1.7.10
+
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class Iterate {
 
-	public static final boolean[] trueAndFalse = {true, false};
-	public static final boolean[] falseAndTrue = {false, true};
-	public static final int[] zeroAndOne = {0, 1};
-	public static final int[] positiveAndNegative = {1, -1};
-	public static final Direction[] directions = Direction.values();
-	public static final Direction[] horizontalDirections = getHorizontals();
-	public static final Axis[] axes = Axis.values();
-	public static final EnumSet<Axis> axisSet = EnumSet.allOf(Axis.class);
+    public static final boolean[] trueAndFalse = {true, false};
+    public static final boolean[] falseAndTrue = {false, true};
+    public static final int[] zeroAndOne = {0, 1};
+    public static final int[] positiveAndNegative = {1, -1};
 
-	private static Direction[] getHorizontals() {
-		Direction[] directions = new Direction[4];
-		for (int i = 0; i < 4; i++)
-			directions[i] = Direction.from2DDataValue(i);
-		return directions;
-	}
+    // ForgeDirection.values() in 1.7.10
+    public static final ForgeDirection[] directions = ForgeDirection.values();
+    public static final ForgeDirection[] horizontalDirections = {
+        ForgeDirection.NORTH, ForgeDirection.SOUTH, ForgeDirection.WEST, ForgeDirection.EAST
+    };
 
-	public static Direction[] directionsInAxis(Axis axis) {
-		switch (axis) {
-			case X:
-				return new Direction[]{Direction.EAST, Direction.WEST};
-			case Y:
-				return new Direction[]{Direction.UP, Direction.DOWN};
-			default:
-			case Z:
-				return new Direction[]{Direction.SOUTH, Direction.NORTH};
-		}
-	}
+    public static ForgeDirection[] directionsInAxis(int axis) {
+        switch (axis) {
+            case 0: // X
+                return new ForgeDirection[]{ForgeDirection.EAST, ForgeDirection.WEST};
+            case 1: // Y
+                return new ForgeDirection[]{ForgeDirection.UP, ForgeDirection.DOWN};
+            default: // Z
+                return new ForgeDirection[]{ForgeDirection.SOUTH, ForgeDirection.NORTH};
+        }
+    }
 
-	public static List<BlockPos> hereAndBelow(BlockPos pos) {
-		return Arrays.asList(pos, pos.below());
-	}
+    // BlockPos.below/above -> use y-1, y+1 in 1.7.10
+    public static List<int[]> hereAndBelow(int x, int y, int z) {
+        return Arrays.asList(new int[]{x, y, z}, new int[]{x, y-1, z});
+    }
 
-	public static List<BlockPos> hereBelowAndAbove(BlockPos pos) {
-		return Arrays.asList(pos, pos.below(), pos.above());
-	}
+    public static List<int[]> hereBelowAndAbove(int x, int y, int z) {
+        return Arrays.asList(new int[]{x, y, z}, new int[]{x, y-1, z}, new int[]{x, y+1, z});
+    }
 
-	public static <T> T cycleValue(List<T> list, T current) {
-		int currentIndex = list.indexOf(current);
-		if (currentIndex == -1) {
-			throw new IllegalArgumentException("Current value not found in list");
-		}
-		int nextIndex = (currentIndex + 1) % list.size();
-		return list.get(nextIndex);
-	}
-
+    public static <T> T cycleValue(List<T> list, T current) {
+        int currentIndex = list.indexOf(current);
+        if (currentIndex == -1)
+            throw new IllegalArgumentException("Current value not found in list");
+        return list.get((currentIndex + 1) % list.size());
+    }
 }
