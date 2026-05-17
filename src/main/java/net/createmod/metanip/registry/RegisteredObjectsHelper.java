15e3e116396811ecc752ddb2a139853438e8b702
Apostrcfo2 <temp24919@gmail.com>
2026-05-17 09:36:27 -0300

    metanip: port registry/RegisteredObjectsHelper to 1.7.10 using GameData/Forge registry

--- src/main/java/net/createmod/metanip/registry/RegisteredObjectsHelper.java
+++ src/main/java/net/createmod/metanip/registry/RegisteredObjectsHelper.java
@@ -2,91 +2,63 @@ package net.createmod.metanip.registry;
 
 import javax.annotation.Nullable;
 
-import net.minecraft.core.Registry;
-import net.minecraft.core.particles.ParticleType;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.material.Fluid;
+// import net.minecraft.core.Registry; // not available in 1.7.10
+// import net.minecraft.core.registries.BuiltInRegistries; // not available in 1.7.10
+// import net.minecraft.resources.ResourceLocation; // different package in 1.7.10
+// import net.minecraft.world.item.Item; // different package in 1.7.10
+// import net.minecraft.world.item.Items; // different package in 1.7.10
+// import net.minecraft.world.level.ItemLike; // not available in 1.7.10
+// import net.minecraft.world.level.block.Block; // different package in 1.7.10
+// import net.minecraft.world.level.block.Blocks; // different package in 1.7.10
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import cpw.mods.fml.common.registry.GameData;
 
 public class RegisteredObjectsHelper {
-	public static <V> ResourceLocation getKeyOrThrow(Registry<V> registry, V value) {
-		ResourceLocation key = registry.getKey(value);
-		if (key == null) {
-			throw new IllegalArgumentException("Could not get key for value " + value + "!");
-		}
-		return key;
-	}
 
-	public static ResourceLocation getKeyOrThrow(Block value) {
-		return getKeyOrThrow(BuiltInRegistries.BLOCK, value);
-	}
+    public static ResourceLocation getKeyOrThrow(Block block) {
+        // GameData is the Forge registry in 1.7.10
+        String name = GameData.getBlockRegistry().getNameForObject(block).toString();
+        if (name == null)
+            throw new IllegalArgumentException("Could not get key for block " + block + "!");
+        return new ResourceLocation(name);
+    }
 
-	public static ResourceLocation getKeyOrThrow(Item value) {
-		return getKeyOrThrow(BuiltInRegistries.ITEM, value);
-	}
+    public static ResourceLocation getKeyOrThrow(Item item) {
+        String name = GameData.getItemRegistry().getNameForObject(item).toString();
+        if (name == null)
+            throw new IllegalArgumentException("Could not get key for item " + item + "!");
+        return new ResourceLocation(name);
+    }
 
-	public static ResourceLocation getKeyOrThrow(Fluid value) {
-		return getKeyOrThrow(BuiltInRegistries.FLUID, value);
-	}
+    public static Item getItem(ResourceLocation location) {
+        return (Item) GameData.getItemRegistry().getObject(location.toString());
+    }
 
-	public static ResourceLocation getKeyOrThrow(EntityType<?> value) {
-		return getKeyOrThrow(BuiltInRegistries.ENTITY_TYPE, value);
-	}
+    public static Block getBlock(ResourceLocation location) {
+        return (Block) GameData.getBlockRegistry().getObject(location.toString());
+    }
 
-	public static ResourceLocation getKeyOrThrow(BlockEntityType<?> value) {
-		return getKeyOrThrow(BuiltInRegistries.BLOCK_ENTITY_TYPE, value);
-	}
+    @Nullable
+    public static Object getItemOrBlock(ResourceLocation location) {
+        Item item = getItem(location);
+        if (item != null)
+            return item;
 
-	public static ResourceLocation getKeyOrThrow(Potion value) {
-		return getKeyOrThrow(BuiltInRegistries.POTION, value);
-	}
+        Block block = getBlock(location);
+        if (block != null)
+            return block;
 
-	public static ResourceLocation getKeyOrThrow(ParticleType<?> value) {
-		return getKeyOrThrow(BuiltInRegistries.PARTICLE_TYPE, value);
-	}
+        return null;
+    }
 
-	public static ResourceLocation getKeyOrThrow(RecipeSerializer<?> value) {
-		return getKeyOrThrow(BuiltInRegistries.RECIPE_SERIALIZER, value);
-	}
-
-	public static Item getItem(ResourceLocation location) {
-		return BuiltInRegistries.ITEM.get(location);
-	}
-
-	public static Block getBlock(ResourceLocation location) {
-		return BuiltInRegistries.BLOCK.get(location);
-	}
-
-	@Nullable
-	public static ItemLike getItemOrBlock(ResourceLocation location) {
-		Item item = getItem(location);
-		if (item != Items.AIR)
-			return item;
-
-		Block block = getBlock(location);
-		if (block != Blocks.AIR)
-			return block;
-
-		return null;
-	}
-
-	public static ResourceLocation getKeyOrThrow(ItemLike itemLike) {
-		if (itemLike instanceof Item item) {
-			return getKeyOrThrow(item);
-		} else if (itemLike instanceof Block block) {
-			return getKeyOrThrow(block);
-		}
-
-		throw new IllegalArgumentException("Could not get key for itemLike " + itemLike + "!");
-	}
-
+    public static ResourceLocation getKeyOrThrow(Object itemOrBlock) {
+        if (itemOrBlock instanceof Item)
+            return getKeyOrThrow((Item) itemOrBlock);
+        if (itemOrBlock instanceof Block)
+            return getKeyOrThrow((Block) itemOrBlock);
+        throw new IllegalArgumentException("Could not get key for " + itemOrBlock + "!");
+    }
 }
