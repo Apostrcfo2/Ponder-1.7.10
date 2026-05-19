45000afd81d3ca0e7b679d89afd863f00bc33a00
Apostrcfo2 <temp24919@gmail.com>
2026-05-19 18:16:15 -0300

    metanip: port UIRenderHelper and TickableGuiEventListener to 1.7.10 - GL11 rendering

--- src/main/java/net/createmod/metanip/gui/TickableGuiEventListener.java
+++ src/main/java/net/createmod/metanip/gui/TickableGuiEventListener.java
@@ -1,7 +1,8 @@
 package net.createmod.metanip.gui;
 
-import net.minecraft.client.gui.components.events.GuiEventListener;
+// import net.minecraft.client.gui.components.events.GuiEventListener; // not available in 1.7.10
+// In 1.7.10 widgets handle their own ticking
 
-public interface TickableGuiEventListener extends GuiEventListener {
-	void tick();
+public interface TickableGuiEventListener {
+    void tick();
 }
