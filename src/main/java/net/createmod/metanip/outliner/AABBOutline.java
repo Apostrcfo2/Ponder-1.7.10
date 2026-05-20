3e1cd27ef2e514c40d2966e64745dd62bd4ca553
Apostrcfo2 <temp24919@gmail.com>
2026-05-20 19:30:42 -0300

    metanip: port outliner/Outline, Outliner, AABBOutline to 1.7.10 - GL11+JOML, AxisAlignedBB, ForgeDirection

--- src/main/java/net/createmod/metanip/outliner/AABBOutline.java
+++ src/main/java/net/createmod/metanip/outliner/AABBOutline.java
@@ -1,227 +1,163 @@
 package net.createmod.metanip.outliner;
 
+import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.Vec3;
+import net.minecraftforge.common.util.ForgeDirection;
 
-import net.createmod.metanip.render.BindableTexture;
-import net.createmod.metanip.render.PonderRenderTypes;
-import net.createmod.metanip.render.SuperRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.core.Direction;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
+import org.lwjgl.opengl.GL11;
 
 public class AABBOutline extends Outline {
 
-	protected AABB bb;
+    protected AxisAlignedBB bb;
 
-	protected final Vector3f minPosTemp1 = new Vector3f();
-	protected final Vector3f maxPosTemp1 = new Vector3f();
+    protected final Vector3f minPosTemp1 = new Vector3f();
+    protected final Vector3f maxPosTemp1 = new Vector3f();
+    protected final Vector4f colorTemp1 = new Vector4f();
+    protected final Vector3f pos0Temp = new Vector3f();
+    protected final Vector3f pos1Temp = new Vector3f();
+    protected final Vector3f pos2Temp = new Vector3f();
+    protected final Vector3f pos3Temp = new Vector3f();
+    protected final Vector3f normalTemp = new Vector3f();
+    protected final Vector3f originTemp = new Vector3f();
 
-	protected final Vector4f colorTemp1 = new Vector4f();
-	protected final Vector3f pos0Temp = new Vector3f();
-	protected final Vector3f pos1Temp = new Vector3f();
-	protected final Vector3f pos2Temp = new Vector3f();
-	protected final Vector3f pos3Temp = new Vector3f();
-	protected final Vector3f normalTemp = new Vector3f();
-	protected final Vector3f originTemp = new Vector3f();
+    public AABBOutline(AxisAlignedBB bb) { setBounds(bb); }
 
-	public AABBOutline(AABB bb) {
-		setBounds(bb);
-	}
+    public AxisAlignedBB getBounds() { return bb; }
+    public void setBounds(AxisAlignedBB bb) { this.bb = bb; }
 
-	public AABB getBounds() {
-		return bb;
-	}
+    @Override
+    public void render(Matrix4f ms, Vec3 camera, float pt) {
+        params.loadColor(colorTemp);
+        renderBox(ms, camera, bb, colorTemp, params.disableLineNormals);
+    }
 
-	public void setBounds(AABB bb) {
-		this.bb = bb;
-	}
+    protected void renderBox(Matrix4f ms, Vec3 camera, AxisAlignedBB box, Vector4f color, boolean disableLineNormals) {
+        boolean cameraInside = camera.xCoord >= box.minX && camera.xCoord <= box.maxX
+            && camera.yCoord >= box.minY && camera.yCoord <= box.maxY
+            && camera.zCoord >= box.minZ && camera.zCoord <= box.maxZ;
+        float inflate = cameraInside ? -1/128f : 1/128f;
 
-	@Override
-	public void render(PoseStack ms, SuperRenderTypeBuffer buffer, Vec3 camera, float pt) {
-		params.loadColor(colorTemp);
-		Vector4f color = colorTemp;
-		int lightmap = params.lightmap;
-		boolean disableLineNormals = params.disableLineNormals;
-		renderBox(ms, buffer, camera, bb, color, lightmap, disableLineNormals);
-	}
+        // Offset by camera
+        float minX = (float)(box.minX - camera.xCoord) - inflate;
+        float minY = (float)(box.minY - camera.yCoord) - inflate;
+        float minZ = (float)(box.minZ - camera.zCoord) - inflate;
+        float maxX = (float)(box.maxX - camera.xCoord) + inflate;
+        float maxY = (float)(box.maxY - camera.yCoord) + inflate;
+        float maxZ = (float)(box.maxZ - camera.zCoord) + inflate;
 
-	protected void renderBox(PoseStack ms, SuperRenderTypeBuffer buffer, Vec3 camera, AABB box, Vector4f color, int lightmap, boolean disableLineNormals) {
-		Vector3f minPos = minPosTemp1;
-		Vector3f maxPos = maxPosTemp1;
+        minPosTemp1.set(minX, minY, minZ);
+        maxPosTemp1.set(maxX, maxY, maxZ);
 
-		boolean cameraInside = box.contains(camera);
-		boolean cull = !cameraInside && !params.disableCull;
-		float inflate = cameraInside ? -1 / 128f : 1 / 128f;
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
 
-		box = box.move(camera.scale(-1));
-		minPos.set((float) box.minX - inflate, (float) box.minY - inflate, (float) box.minZ - inflate);
-		maxPos.set((float) box.maxX + inflate, (float) box.maxY + inflate, (float) box.maxZ + inflate);
+        if (!cameraInside && !params.disableCull)
+            GL11.glEnable(GL11.GL_CULL_FACE);
 
-		renderBoxFaces(ms, buffer, cull, params.getHighlightedFace(), minPos, maxPos, color, lightmap);
+        // Render faces if texture set
+        if (params.faceTexturePath != null)
+            renderBoxFaces(minPosTemp1, maxPosTemp1, color);
 
-		float lineWidth = params.getLineWidth();
-		if (lineWidth == 0)
-			return;
+        // Render edges
+        float lineWidth = params.getLineWidth();
+        if (lineWidth > 0) {
+            GL11.glLineWidth(Math.max(1, (int)(lineWidth * 32)));
+            renderBoxEdges(minPosTemp1, maxPosTemp1, lineWidth, color, disableLineNormals);
+        }
 
-		VertexConsumer consumer = buffer.getBuffer(PonderRenderTypes.outlineSolid());
-		renderBoxEdges(ms, consumer, minPos, maxPos, lineWidth, color, lightmap, disableLineNormals);
-	}
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_CULL_FACE);
+    }
 
-	protected void renderBoxFaces(PoseStack ms, SuperRenderTypeBuffer buffer, boolean cull, Direction highlightedFace, Vector3f minPos, Vector3f maxPos, Vector4f color, int lightmap) {
-		PoseStack.Pose pose = ms.last();
-		renderBoxFace(pose, buffer, cull, highlightedFace, minPos, maxPos, Direction.DOWN, color, lightmap);
-		renderBoxFace(pose, buffer, cull, highlightedFace, minPos, maxPos, Direction.UP, color, lightmap);
-		renderBoxFace(pose, buffer, cull, highlightedFace, minPos, maxPos, Direction.NORTH, color, lightmap);
-		renderBoxFace(pose, buffer, cull, highlightedFace, minPos, maxPos, Direction.SOUTH, color, lightmap);
-		renderBoxFace(pose, buffer, cull, highlightedFace, minPos, maxPos, Direction.WEST, color, lightmap);
-		renderBoxFace(pose, buffer, cull, highlightedFace, minPos, maxPos, Direction.EAST, color, lightmap);
-	}
+    protected void renderBoxFaces(Vector3f minPos, Vector3f maxPos, Vector4f color) {
+        for (ForgeDirection face : new ForgeDirection[]{
+            ForgeDirection.DOWN, ForgeDirection.UP,
+            ForgeDirection.NORTH, ForgeDirection.SOUTH,
+            ForgeDirection.WEST, ForgeDirection.EAST
+        }) {
+            renderBoxFace(minPos, maxPos, face, color);
+        }
+    }
 
-	protected void renderBoxFace(PoseStack.Pose pose, SuperRenderTypeBuffer buffer, boolean cull, Direction highlightedFace, Vector3f minPos, Vector3f maxPos, Direction face, Vector4f color, int lightmap) {
-		boolean highlighted = face == highlightedFace;
+    protected void renderBoxFace(Vector3f minPos, Vector3f maxPos, ForgeDirection face, Vector4f color) {
+        float minX = minPos.x(), minY = minPos.y(), minZ = minPos.z();
+        float maxX = maxPos.x(), maxY = maxPos.y(), maxZ = maxPos.z();
 
-		// TODO: Presumably, the other texture should be used, but this was not noticed before so fixing it may lead to suboptimal visuals.
-		//BindableTexture faceTexture = highlighted ? params.hightlightedFaceTexture : params.faceTexture;
-		BindableTexture faceTexture = params.faceTexture;
-		if (faceTexture == null)
-			return;
+        boolean highlighted = face == params.highlightedFace;
+        float alphaMult = highlighted ? 1 : 0.5f;
+        colorTemp1.set(color.x(), color.y(), color.z(), color.w() * alphaMult);
 
-		RenderType renderType = PonderRenderTypes.outlineTranslucent(faceTexture.getLocation(), cull);
-		VertexConsumer consumer = buffer.getLateBuffer(renderType);
+        Vector3f p0 = pos0Temp, p1 = pos1Temp, p2 = pos2Temp, p3 = pos3Temp;
+        Vector3f n = normalTemp;
 
-		float alphaMult = highlighted ? 1 : 0.5f;
-		colorTemp1.set(color.x(), color.y(), color.z(), color.w() * alphaMult);
-		color = colorTemp1;
+        switch (face) {
+            case DOWN:
+                p0.set(minX, minY, maxZ); p1.set(minX, minY, minZ);
+                p2.set(maxX, minY, minZ); p3.set(maxX, minY, maxZ);
+                n.set(0, -1, 0); break;
+            case UP:
+                p0.set(minX, maxY, minZ); p1.set(minX, maxY, maxZ);
+                p2.set(maxX, maxY, maxZ); p3.set(maxX, maxY, minZ);
+                n.set(0, 1, 0); break;
+            case NORTH:
+                p0.set(maxX, maxY, minZ); p1.set(maxX, minY, minZ);
+                p2.set(minX, minY, minZ); p3.set(minX, maxY, minZ);
+                n.set(0, 0, -1); break;
+            case SOUTH:
+                p0.set(minX, maxY, maxZ); p1.set(minX, minY, maxZ);
+                p2.set(maxX, minY, maxZ); p3.set(maxX, maxY, maxZ);
+                n.set(0, 0, 1); break;
+            case WEST:
+                p0.set(minX, maxY, minZ); p1.set(minX, minY, minZ);
+                p2.set(minX, minY, maxZ); p3.set(minX, maxY, maxZ);
+                n.set(-1, 0, 0); break;
+            case EAST:
+                p0.set(maxX, maxY, maxZ); p1.set(maxX, minY, maxZ);
+                p2.set(maxX, minY, minZ); p3.set(maxX, maxY, minZ);
+                n.set(1, 0, 0); break;
+            default: return;
+        }
 
-		renderBoxFace(pose, consumer, minPos, maxPos, face, color, lightmap);
-	}
+        bufferQuad(p0, p1, p2, p3, colorTemp1, n);
+    }
 
-	protected void renderBoxFace(PoseStack.Pose pose, VertexConsumer consumer, Vector3f minPos, Vector3f maxPos, Direction face, Vector4f color, int lightmap) {
-		Vector3f pos0 = pos0Temp;
-		Vector3f pos1 = pos1Temp;
-		Vector3f pos2 = pos2Temp;
-		Vector3f pos3 = pos3Temp;
-		Vector3f normal = normalTemp;
+    protected void renderBoxEdges(Vector3f minPos, Vector3f maxPos, float lineWidth, Vector4f color, boolean disableNormals) {
+        Vector3f origin = originTemp;
 
-		float minX = minPos.x();
-		float minY = minPos.y();
-		float minZ = minPos.z();
-		float maxX = maxPos.x();
-		float maxY = maxPos.y();
-		float maxZ = maxPos.z();
+        float lx = maxPos.x() - minPos.x();
+        float ly = maxPos.y() - minPos.y();
+        float lz = maxPos.z() - minPos.z();
 
-		float maxU;
-		float maxV;
+        origin.set(minPos);
+        bufferCuboidLine(origin, ForgeDirection.EAST, lx, lineWidth, color, disableNormals);
+        bufferCuboidLine(origin, ForgeDirection.UP, ly, lineWidth, color, disableNormals);
+        bufferCuboidLine(origin, ForgeDirection.SOUTH, lz, lineWidth, color, disableNormals);
 
-		switch (face) {
-			case DOWN -> {
-				// 0 1 2 3
-				pos0.set(minX, minY, maxZ);
-				pos1.set(minX, minY, minZ);
-				pos2.set(maxX, minY, minZ);
-				pos3.set(maxX, minY, maxZ);
-				maxU = maxX - minX;
-				maxV = maxZ - minZ;
-				normal.set(0, -1, 0);
-			}
-			case UP -> {
-				// 4 5 6 7
-				pos0.set(minX, maxY, minZ);
-				pos1.set(minX, maxY, maxZ);
-				pos2.set(maxX, maxY, maxZ);
-				pos3.set(maxX, maxY, minZ);
-				maxU = maxX - minX;
-				maxV = maxZ - minZ;
-				normal.set(0, 1, 0);
-			}
-			case NORTH -> {
-				// 7 2 1 4
-				pos0.set(maxX, maxY, minZ);
-				pos1.set(maxX, minY, minZ);
-				pos2.set(minX, minY, minZ);
-				pos3.set(minX, maxY, minZ);
-				maxU = maxX - minX;
-				maxV = maxY - minY;
-				normal.set(0, 0, -1);
-			}
-			case SOUTH -> {
-				// 5 0 3 6
-				pos0.set(minX, maxY, maxZ);
-				pos1.set(minX, minY, maxZ);
-				pos2.set(maxX, minY, maxZ);
-				pos3.set(maxX, maxY, maxZ);
-				maxU = maxX - minX;
-				maxV = maxY - minY;
-				normal.set(0, 0, 1);
-			}
-			case WEST -> {
-				// 4 1 0 5
-				pos0.set(minX, maxY, minZ);
-				pos1.set(minX, minY, minZ);
-				pos2.set(minX, minY, maxZ);
-				pos3.set(minX, maxY, maxZ);
-				maxU = maxZ - minZ;
-				maxV = maxY - minY;
-				normal.set(-1, 0, 0);
-			}
-			case EAST -> {
-				// 6 3 2 7
-				pos0.set(maxX, maxY, maxZ);
-				pos1.set(maxX, minY, maxZ);
-				pos2.set(maxX, minY, minZ);
-				pos3.set(maxX, maxY, minZ);
-				maxU = maxZ - minZ;
-				maxV = maxY - minY;
-				normal.set(1, 0, 0);
-			}
-			default -> {
-				maxU = 1;
-				maxV = 1;
-			}
-		}
+        origin.set(maxPos.x(), minPos.y(), minPos.z());
+        bufferCuboidLine(origin, ForgeDirection.UP, ly, lineWidth, color, disableNormals);
+        bufferCuboidLine(origin, ForgeDirection.SOUTH, lz, lineWidth, color, disableNormals);
 
-		bufferQuad(pose, consumer, pos0, pos1, pos2, pos3, color, 0, 0, maxU, maxV, lightmap, normal);
-	}
+        origin.set(minPos.x(), maxPos.y(), minPos.z());
+        bufferCuboidLine(origin, ForgeDirection.EAST, lx, lineWidth, color, disableNormals);
+        bufferCuboidLine(origin, ForgeDirection.SOUTH, lz, lineWidth, color, disableNormals);
 
-	protected void renderBoxEdges(PoseStack ms, VertexConsumer consumer, Vector3f minPos, Vector3f maxPos, float lineWidth, Vector4f color, int lightmap, boolean disableNormals) {
-		Vector3f origin = originTemp;
+        origin.set(minPos.x(), minPos.y(), maxPos.z());
+        bufferCuboidLine(origin, ForgeDirection.EAST, lx, lineWidth, color, disableNormals);
+        bufferCuboidLine(origin, ForgeDirection.UP, ly, lineWidth, color, disableNormals);
 
-		PoseStack.Pose pose = ms.last();
+        origin.set(minPos.x(), maxPos.y(), maxPos.z());
+        bufferCuboidLine(origin, ForgeDirection.EAST, lx, lineWidth, color, disableNormals);
 
-		float lineLengthX = maxPos.x() - minPos.x();
-		float lineLengthY = maxPos.y() - minPos.y();
-		float lineLengthZ = maxPos.z() - minPos.z();
+        origin.set(maxPos.x(), minPos.y(), maxPos.z());
+        bufferCuboidLine(origin, ForgeDirection.UP, ly, lineWidth, color, disableNormals);
 
-		origin.set(minPos);
-		bufferCuboidLine(pose, consumer, origin, Direction.EAST, lineLengthX, lineWidth, color, lightmap, disableNormals);
-		bufferCuboidLine(pose, consumer, origin, Direction.UP, lineLengthY, lineWidth, color, lightmap, disableNormals);
-		bufferCuboidLine(pose, consumer, origin, Direction.SOUTH, lineLengthZ, lineWidth, color, lightmap, disableNormals);
-
-		origin.set(maxPos.x(), minPos.y(), minPos.z());
-		bufferCuboidLine(pose, consumer, origin, Direction.UP, lineLengthY, lineWidth, color, lightmap, disableNormals);
-		bufferCuboidLine(pose, consumer, origin, Direction.SOUTH, lineLengthZ, lineWidth, color, lightmap, disableNormals);
-
-		origin.set(minPos.x(), maxPos.y(), minPos.z());
-		bufferCuboidLine(pose, consumer, origin, Direction.EAST, lineLengthX, lineWidth, color, lightmap, disableNormals);
-		bufferCuboidLine(pose, consumer, origin, Direction.SOUTH, lineLengthZ, lineWidth, color, lightmap, disableNormals);
-
-		origin.set(minPos.x(), minPos.y(), maxPos.z());
-		bufferCuboidLine(pose, consumer, origin, Direction.EAST, lineLengthX, lineWidth, color, lightmap, disableNormals);
-		bufferCuboidLine(pose, consumer, origin, Direction.UP, lineLengthY, lineWidth, color, lightmap, disableNormals);
-
-		origin.set(minPos.x(), maxPos.y(), maxPos.z());
-		bufferCuboidLine(pose, consumer, origin, Direction.EAST, lineLengthX, lineWidth, color, lightmap, disableNormals);
-
-		origin.set(maxPos.x(), minPos.y(), maxPos.z());
-		bufferCuboidLine(pose, consumer, origin, Direction.UP, lineLengthY, lineWidth, color, lightmap, disableNormals);
-
-		origin.set(maxPos.x(), maxPos.y(), minPos.z());
-		bufferCuboidLine(pose, consumer, origin, Direction.SOUTH, lineLengthZ, lineWidth, color, lightmap, disableNormals);
-	}
-
+        origin.set(maxPos.x(), maxPos.y(), minPos.z());
+        bufferCuboidLine(origin, ForgeDirection.SOUTH, lz, lineWidth, color, disableNormals);
+    }
 }
--- src/main/java/net/createmod/metanip/outliner/Outliner.java
+++ src/main/java/net/createmod/metanip/outliner/Outliner.java
@@ -6,209 +6,148 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Optional;
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import org.joml.Matrix4f;
 
 import net.createmod.metanip.outliner.LineOutline.EndChasingLineOutline;
 import net.createmod.metanip.outliner.Outline.OutlineParams;
-import net.createmod.metanip.render.SuperRenderTypeBuffer;
-import net.minecraft.core.BlockPos;
-import net.minecraft.util.Mth;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.Vec3;
 
 public class Outliner {
 
-	// Singleton
+    private static final Outliner instance = new Outliner();
+    public static Outliner getInstance() { return instance; }
 
-	private static final Outliner instance = new Outliner();
+    public Outliner() {}
 
-	public static Outliner getInstance() {
-		return instance;
-	}
+    private final Map<Object, OutlineEntry> outlines = Collections.synchronizedMap(new HashMap<>());
+    private final Map<Object, OutlineEntry> outlinesView = Collections.unmodifiableMap(outlines);
 
-	//
+    public OutlineParams showOutline(Object slot, Outline outline) {
+        outlines.put(slot, new OutlineEntry(outline));
+        return outline.getParams();
+    }
 
-	public Outliner() {
-	}
+    public OutlineParams showLine(Object slot, Vec3 start, Vec3 end) {
+        if (!outlines.containsKey(slot))
+            addOutline(slot, new LineOutline());
+        OutlineEntry entry = outlines.get(slot);
+        entry.ticksTillRemoval = 1;
+        ((LineOutline) entry.outline).set(start, end);
+        return entry.outline.getParams();
+    }
 
-	private final Map<Object, OutlineEntry> outlines = Collections.synchronizedMap(new HashMap<>());
-	private final Map<Object, OutlineEntry> outlinesView = Collections.unmodifiableMap(outlines);
+    public OutlineParams endChasingLine(Object slot, Vec3 start, Vec3 end, float chasingProgress, boolean lockStart) {
+        if (!outlines.containsKey(slot))
+            addOutline(slot, new EndChasingLineOutline(lockStart));
+        OutlineEntry entry = outlines.get(slot);
+        entry.ticksTillRemoval = 1;
+        ((EndChasingLineOutline) entry.outline).setProgress(chasingProgress).set(start, end);
+        return entry.outline.getParams();
+    }
 
-	// Facade
+    public OutlineParams showAABB(Object slot, AxisAlignedBB bb, int ttl) {
+        createAABBOutlineIfMissing(slot, bb);
+        ChasingAABBOutline outline = getAndRefreshAABB(slot, ttl);
+        outline.prevBB = outline.targetBB = outline.bb = bb;
+        return outline.getParams();
+    }
 
-	public OutlineParams showOutline(Object slot, Outline outline) {
-		outlines.put(slot, new OutlineEntry(outline));
-		return outline.getParams();
-	}
+    public OutlineParams showAABB(Object slot, AxisAlignedBB bb) {
+        return showAABB(slot, bb, 1);
+    }
 
-	public OutlineParams showLine(Object slot, Vec3 start, Vec3 end) {
-		if (!outlines.containsKey(slot)) {
-			LineOutline outline = new LineOutline();
-			addOutline(slot, outline);
-		}
-		OutlineEntry entry = outlines.get(slot);
-		entry.ticksTillRemoval = 1;
-		((LineOutline) entry.outline).set(start, end);
-		return entry.outline.getParams();
-	}
+    public OutlineParams chaseAABB(Object slot, AxisAlignedBB bb) {
+        createAABBOutlineIfMissing(slot, bb);
+        ChasingAABBOutline outline = getAndRefreshAABB(slot);
+        outline.targetBB = bb;
+        return outline.getParams();
+    }
 
-	public OutlineParams endChasingLine(Object slot, Vec3 start, Vec3 end, float chasingProgress, boolean lockStart) {
-		if (!outlines.containsKey(slot)) {
-			EndChasingLineOutline outline = new EndChasingLineOutline(lockStart);
-			addOutline(slot, outline);
-		}
-		OutlineEntry entry = outlines.get(slot);
-		entry.ticksTillRemoval = 1;
-		((EndChasingLineOutline) entry.outline).setProgress(chasingProgress)
-			.set(start, end);
-		return entry.outline.getParams();
-	}
+    public OutlineParams showCluster(Object slot, Iterable<int[]> selection) {
+        BlockClusterOutline outline = new BlockClusterOutline(selection);
+        addOutline(slot, outline);
+        return outline.getParams();
+    }
 
-	public OutlineParams showAABB(Object slot, AABB bb, int ttl) {
-		createAABBOutlineIfMissing(slot, bb);
-		ChasingAABBOutline outline = getAndRefreshAABB(slot, ttl);
-		outline.prevBB = outline.targetBB = outline.bb = bb;
-		return outline.getParams();
-	}
+    public OutlineParams showItem(Object slot, Vec3 pos, ItemStack stack) {
+        ItemOutline outline = new ItemOutline(pos, stack);
+        OutlineEntry entry = new OutlineEntry(outline);
+        outlines.put(slot, entry);
+        return entry.getOutline().getParams();
+    }
 
-	public OutlineParams showAABB(Object slot, AABB bb) {
-		createAABBOutlineIfMissing(slot, bb);
-		ChasingAABBOutline outline = getAndRefreshAABB(slot);
-		outline.prevBB = outline.targetBB = outline.bb = bb;
-		return outline.getParams();
-	}
+    public void keep(Object slot) {
+        if (outlines.containsKey(slot))
+            outlines.get(slot).ticksTillRemoval = 1;
+    }
 
-	public OutlineParams chaseAABB(Object slot, AABB bb) {
-		createAABBOutlineIfMissing(slot, bb);
-		ChasingAABBOutline outline = getAndRefreshAABB(slot);
-		outline.targetBB = bb;
-		return outline.getParams();
-	}
+    public void remove(Object slot) { outlines.remove(slot); }
 
-	public OutlineParams showCluster(Object slot, Iterable<BlockPos> selection) {
-		BlockClusterOutline outline = new BlockClusterOutline(selection);
-		addOutline(slot, outline);
-		return outline.getParams();
-	}
+    public Optional<OutlineParams> edit(Object slot) {
+        keep(slot);
+        if (outlines.containsKey(slot))
+            return Optional.of(outlines.get(slot).getOutline().getParams());
+        return Optional.empty();
+    }
 
-	//
+    public Map<Object, OutlineEntry> getOutlines() { return outlinesView; }
 
-	public OutlineParams showItem(Object slot, Vec3 pos, ItemStack stack) {
-		ItemOutline outline = new ItemOutline(pos, stack);
-		OutlineEntry entry = new OutlineEntry(outline);
-		outlines.put(slot, entry);
-		return entry.getOutline().getParams();
-	}
+    private void addOutline(Object slot, Outline outline) {
+        outlines.put(slot, new OutlineEntry(outline));
+    }
 
-	public void keep(Object slot) {
-		if (outlines.containsKey(slot))
-			outlines.get(slot).ticksTillRemoval = 1;
-	}
+    private void createAABBOutlineIfMissing(Object slot, AxisAlignedBB bb) {
+        if (!outlines.containsKey(slot) || !(outlines.get(slot).outline instanceof AABBOutline))
+            addOutline(slot, new ChasingAABBOutline(bb));
+    }
 
-	public void remove(Object slot) {
-		outlines.remove(slot);
-	}
+    private ChasingAABBOutline getAndRefreshAABB(Object slot) { return getAndRefreshAABB(slot, 1); }
 
-	public Optional<OutlineParams> edit(Object slot) {
-		keep(slot);
-		if (outlines.containsKey(slot))
-			return Optional.of(outlines.get(slot)
-				.getOutline()
-				.getParams());
-		return Optional.empty();
-	}
+    private ChasingAABBOutline getAndRefreshAABB(Object slot, int ttl) {
+        OutlineEntry entry = outlines.get(slot);
+        entry.ticksTillRemoval = ttl;
+        return (ChasingAABBOutline) entry.getOutline();
+    }
 
-	public Map<Object, OutlineEntry> getOutlines() {
-		return outlinesView;
-	}
+    public void tickOutlines() {
+        Iterator<OutlineEntry> iterator = outlines.values().iterator();
+        while (iterator.hasNext()) {
+            OutlineEntry entry = iterator.next();
+            entry.tick();
+            if (!entry.isAlive()) iterator.remove();
+        }
+    }
 
-	// Utility
+    public void renderOutlines(Matrix4f ms, Vec3 camera, float pt) {
+        outlines.forEach((key, entry) -> {
+            Outline outline = entry.getOutline();
+            OutlineParams params = outline.getParams();
+            params.alpha = 1;
+            if (entry.isFading()) {
+                int prevTicks = entry.ticksTillRemoval + 1;
+                float fadeticks = OutlineEntry.FADE_TICKS;
+                float lastAlpha = prevTicks >= 0 ? 1 : 1 + (prevTicks / fadeticks);
+                float currentAlpha = 1 + (entry.ticksTillRemoval / fadeticks);
+                float alpha = lastAlpha + (currentAlpha - lastAlpha) * pt;
+                params.alpha = alpha * alpha * alpha;
+                if (params.alpha < 1 / 8f) return;
+            }
+            outline.render(ms, camera, pt);
+        });
+    }
 
-	private void addOutline(Object slot, Outline outline) {
-		outlines.put(slot, new OutlineEntry(outline));
-	}
+    public static class OutlineEntry {
+        public static final int FADE_TICKS = 8;
+        private final Outline outline;
+        int ticksTillRemoval = 1;
 
-	private void createAABBOutlineIfMissing(Object slot, AABB bb) {
-		if (!outlines.containsKey(slot) || !(outlines.get(slot).outline instanceof AABBOutline)) {
-			ChasingAABBOutline outline = new ChasingAABBOutline(bb);
-			addOutline(slot, outline);
-		}
-	}
-
-	private ChasingAABBOutline getAndRefreshAABB(Object slot) {
-		return getAndRefreshAABB(slot, 1);
-	}
-
-	private ChasingAABBOutline getAndRefreshAABB(Object slot, int ttl) {
-		OutlineEntry entry = outlines.get(slot);
-		entry.ticksTillRemoval = ttl;
-		return (ChasingAABBOutline) entry.getOutline();
-	}
-
-	// Maintenance
-
-	public void tickOutlines() {
-		Iterator<OutlineEntry> iterator = outlines.values()
-			.iterator();
-		while (iterator.hasNext()) {
-			OutlineEntry entry = iterator.next();
-			entry.tick();
-			if (!entry.isAlive())
-				iterator.remove();
-		}
-	}
-
-	public void renderOutlines(PoseStack ms, SuperRenderTypeBuffer buffer, Vec3 camera, float pt) {
-		outlines.forEach((key, entry) -> {
-			Outline outline = entry.getOutline();
-			OutlineParams params = outline.getParams();
-			params.alpha = 1;
-			if (entry.isFading()) {
-				int prevTicks = entry.ticksTillRemoval + 1;
-				float fadeticks = OutlineEntry.FADE_TICKS;
-				float lastAlpha = prevTicks >= 0 ? 1 : 1 + (prevTicks / fadeticks);
-				float currentAlpha = 1 + (entry.ticksTillRemoval / fadeticks);
-				float alpha = Mth.lerp(pt, lastAlpha, currentAlpha);
-
-				params.alpha = alpha * alpha * alpha;
-				if (params.alpha < 1 / 8f)
-					return;
-			}
-			outline.render(ms, buffer, camera, pt);
-		});
-	}
-
-	public static class OutlineEntry {
-		public static final int FADE_TICKS = 8;
-
-		private final Outline outline;
-		private int ticksTillRemoval = 1;
-
-		public OutlineEntry(Outline outline) {
-			this.outline = outline;
-		}
-
-		public Outline getOutline() {
-			return outline;
-		}
-
-		public int getTicksTillRemoval() {
-			return ticksTillRemoval;
-		}
-
-		public boolean isAlive() {
-			return ticksTillRemoval >= -FADE_TICKS;
-		}
-
-		public boolean isFading() {
-			return ticksTillRemoval < 0;
-		}
-
-		public void tick() {
-			ticksTillRemoval--;
-			outline.tick();
-		}
-	}
-
+        public OutlineEntry(Outline outline) { this.outline = outline; }
+        public Outline getOutline() { return outline; }
+        public int getTicksTillRemoval() { return ticksTillRemoval; }
+        public boolean isAlive() { return ticksTillRemoval >= -FADE_TICKS; }
+        public boolean isFading() { return ticksTillRemoval < 0; }
+        public void tick() { ticksTillRemoval--; outline.tick(); }
+    }
 }
