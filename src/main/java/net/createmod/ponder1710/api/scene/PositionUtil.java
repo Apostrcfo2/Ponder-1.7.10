d73356a82324ed83a72fcf6f957e51932d6a215f
Apostrcfo2 <temp24919@gmail.com>
2026-05-31 18:55:13 -0300

    phase3: review api/scene - clean comments, SelectionUtil BlockPos->xyz

--- src/main/java/net/createmod/ponder1710/api/scene/PositionUtil.java
+++ src/main/java/net/createmod/ponder1710/api/scene/PositionUtil.java
@@ -1,11 +1,8 @@
 package net.createmod.ponder1710.api.scene;
 
-// import net.minecraft.core.BlockPos; // 1.7.10 uses x,y,z or ChunkCoordinates
-// Replaced with int[] {x, y, z}
-
+// BlockPos -> int[] {x,y,z} in 1.7.10
 public interface PositionUtil {
 
-    // BlockPos -> int[] {x, y, z} in 1.7.10
     int[] at(int x, int y, int z);
 
     int[] zero();
--- src/main/java/net/createmod/ponder1710/api/scene/SelectionUtil.java
+++ src/main/java/net/createmod/ponder1710/api/scene/SelectionUtil.java
@@ -1,19 +1,14 @@
 package net.createmod.ponder1710.api.scene;
 
-// import net.minecraft.core.BlockPos; // 1.7.10 uses x,y,z
-// import net.minecraft.core.Vec3i; // not available in 1.7.10
-
+// BlockPos -> x,y,z ints in 1.7.10
+// Vec3i -> sizeX,sizeY,sizeZ ints in 1.7.10
 public interface SelectionUtil {
 
     Selection everywhere();
 
     Selection position(int x, int y, int z);
 
-    // Selection position(BlockPos pos); // BlockPos -> x,y,z in 1.7.10
-
     Selection fromTo(int x, int y, int z, int x2, int y2, int z2);
-
-    // Selection fromTo(BlockPos pos1, BlockPos pos2);
 
     Selection column(int x, int z);
 
@@ -23,6 +18,5 @@ public interface SelectionUtil {
 
     Selection layers(int y, int height);
 
-    // Selection cuboid(BlockPos origin, Vec3i size); // replaced below
     Selection cuboid(int originX, int originY, int originZ, int sizeX, int sizeY, int sizeZ);
 }
