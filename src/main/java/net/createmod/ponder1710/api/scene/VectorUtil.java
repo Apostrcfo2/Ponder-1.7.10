a8241cd3810d18d7f3ae17479e05e6aadf0c8824
Apostrcfo2 <temp24919@gmail.com>
2026-06-01 19:03:58 -0300

    phase3: review VectorUtil, WorldInstructions - add missing modifyBlock/cycleBlockProperty/modifyBlocks

--- src/main/java/net/createmod/ponder1710/api/scene/VectorUtil.java
+++ src/main/java/net/createmod/ponder1710/api/scene/VectorUtil.java
@@ -1,23 +1,16 @@
 package net.createmod.ponder1710.api.scene;
 
-// import net.minecraft.core.BlockPos; // 1.7.10 uses x,y,z
-// import net.minecraft.core.Direction; // 1.7.10 uses ForgeDirection
-// import net.minecraft.world.phys.Vec3; // 1.7.10 uses net.minecraft.util.Vec3
 import net.minecraft.util.Vec3;
 import net.minecraftforge.common.util.ForgeDirection;
 
+// BlockPos -> x,y,z ints in 1.7.10
+// Direction -> ForgeDirection in 1.7.10
 public interface VectorUtil {
 
     Vec3 centerOf(int x, int y, int z);
 
-    // BlockPos -> x,y,z in 1.7.10
-    // Vec3 centerOf(BlockPos pos);
-
     Vec3 topOf(int x, int y, int z);
 
-    // Vec3 topOf(BlockPos pos);
-
-    // Direction -> ForgeDirection in 1.7.10
     Vec3 blockSurface(int x, int y, int z, ForgeDirection face);
 
     Vec3 blockSurface(int x, int y, int z, ForgeDirection face, float margin);
--- src/main/java/net/createmod/ponder1710/api/scene/WorldInstructions.java
+++ src/main/java/net/createmod/ponder1710/api/scene/WorldInstructions.java
@@ -8,18 +8,6 @@ import net.createmod.ponder1710.api.element.ElementLink;
 import net.createmod.ponder1710.api.element.EntityElement;
 import net.createmod.ponder1710.api.element.WorldSectionElement;
 
-// import net.minecraft.core.BlockPos; // 1.7.10 uses x,y,z
-// import net.minecraft.core.Direction; // 1.7.10 uses ForgeDirection
-// import net.minecraft.core.HolderLookup; // not available in 1.7.10
-// import net.minecraft.nbt.CompoundTag; // NBTTagCompound in 1.7.10
-// import net.minecraft.world.entity.Entity; // different package in 1.7.10
-// import net.minecraft.world.item.ItemStack; // different package in 1.7.10
-// import net.minecraft.world.level.Level; // World in 1.7.10
-// import net.minecraft.world.level.block.entity.BlockEntity; // TileEntity in 1.7.10
-// import net.minecraft.world.level.block.state.BlockState; // not available in 1.7.10
-// import net.minecraft.world.level.block.state.properties.Property; // not available in 1.7.10
-// import net.minecraft.world.phys.Vec3; // net.minecraft.util.Vec3 in 1.7.10
-
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
@@ -29,33 +17,42 @@ import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
+// BlockPos -> x,y,z ints in 1.7.10
+// Direction -> ForgeDirection in 1.7.10
+// BlockState -> Block + meta in 1.7.10
+// BlockEntity -> TileEntity in 1.7.10
+// CompoundTag -> NBTTagCompound in 1.7.10
+// Level -> World in 1.7.10
+// HolderLookup -> not available in 1.7.10
+// modifyBlock/cycleBlockProperty/modifyBlocks -> adapted for Block+meta system
 public interface WorldInstructions {
-
-    // HolderLookup not available in 1.7.10
-    // HolderLookup.Provider getHolderLookupProvider();
 
     void incrementBlockBreakingProgress(int x, int y, int z);
 
-    // Direction -> ForgeDirection in 1.7.10
     void showSection(Selection selection, ForgeDirection fadeInDirection);
 
-    void showSectionAndMerge(Selection selection, ForgeDirection fadeInDirection, ElementLink<WorldSectionElement> link);
+    void showSectionAndMerge(Selection selection, ForgeDirection fadeInDirection,
+        ElementLink<WorldSectionElement> link);
 
-    void glueBlockOnto(int x, int y, int z, ForgeDirection fadeInDirection, ElementLink<WorldSectionElement> link);
+    void glueBlockOnto(int x, int y, int z, ForgeDirection fadeInDirection,
+        ElementLink<WorldSectionElement> link);
 
-    ElementLink<WorldSectionElement> showIndependentSection(Selection selection, ForgeDirection fadeInDirection);
+    ElementLink<WorldSectionElement> showIndependentSection(Selection selection,
+        ForgeDirection fadeInDirection);
 
     ElementLink<WorldSectionElement> showIndependentSectionImmediately(Selection selection);
 
     void hideSection(Selection selection, ForgeDirection fadeOutDirection);
 
-    void hideIndependentSection(ElementLink<WorldSectionElement> link, ForgeDirection fadeOutDirection);
+    void hideIndependentSection(ElementLink<WorldSectionElement> link,
+        ForgeDirection fadeOutDirection);
 
     void restoreBlocks(Selection selection);
 
     ElementLink<WorldSectionElement> makeSectionIndependent(Selection selection);
 
-    void rotateSection(ElementLink<WorldSectionElement> link, double xRotation, double yRotation, double zRotation, int duration);
+    void rotateSection(ElementLink<WorldSectionElement> link, double xRotation,
+        double yRotation, double zRotation, int duration);
 
     void configureCenterOfRotation(ElementLink<WorldSectionElement> link, Vec3 anchor);
 
@@ -63,7 +60,6 @@ public interface WorldInstructions {
 
     void moveSection(ElementLink<WorldSectionElement> link, Vec3 offset, int duration);
 
-    // BlockState -> Block + meta in 1.7.10
     void setBlocks(Selection selection, Block block, int meta, boolean spawnParticles);
 
     void destroyBlock(int x, int y, int z);
@@ -72,23 +68,34 @@ public interface WorldInstructions {
 
     void replaceBlocks(Selection selection, Block block, int meta, boolean spawnParticles);
 
+    // modifyBlock - applies a function to block+meta at a position
+    void modifyBlock(int x, int y, int z, UnaryOperator<int[]> blockMetaFunc, boolean spawnParticles);
+
+    // cycleBlockProperty - cycles metadata value at position
+    void cycleBlockProperty(int x, int y, int z);
+
+    // modifyBlocks - applies a function to all blocks in selection
+    void modifyBlocks(Selection selection, UnaryOperator<int[]> blockMetaFunc, boolean spawnParticles);
+
     void toggleRedstonePower(Selection selection);
 
     <T extends Entity> void modifyEntities(Class<T> entityClass, Consumer<T> entityCallBack);
 
-    <T extends Entity> void modifyEntitiesInside(Class<T> entityClass, Selection area, Consumer<T> entityCallBack);
+    <T extends Entity> void modifyEntitiesInside(Class<T> entityClass, Selection area,
+        Consumer<T> entityCallBack);
 
     void modifyEntity(ElementLink<EntityElement> link, Consumer<Entity> entityCallBack);
 
-    // Level -> World in 1.7.10
     ElementLink<EntityElement> createEntity(Function<World, Entity> factory);
 
     ElementLink<EntityElement> createItemEntity(Vec3 location, Vec3 motion, ItemStack stack);
 
-    // BlockEntity -> TileEntity, CompoundTag -> NBTTagCompound in 1.7.10
-    void modifyTileEntityNBT(Selection selection, Class<? extends TileEntity> teType, Consumer<NBTTagCompound> consumer);
+    void modifyTileEntityNBT(Selection selection, Class<? extends TileEntity> teType,
+        Consumer<NBTTagCompound> consumer);
 
-    <T extends TileEntity> void modifyTileEntity(int x, int y, int z, Class<T> teType, Consumer<T> consumer);
+    <T extends TileEntity> void modifyTileEntity(int x, int y, int z, Class<T> teType,
+        Consumer<T> consumer);
 
-    void modifyTileEntityNBT(Selection selection, Class<? extends TileEntity> teType, Consumer<NBTTagCompound> consumer, boolean reDrawBlocks);
+    void modifyTileEntityNBT(Selection selection, Class<? extends TileEntity> teType,
+        Consumer<NBTTagCompound> consumer, boolean reDrawBlocks);
 }
