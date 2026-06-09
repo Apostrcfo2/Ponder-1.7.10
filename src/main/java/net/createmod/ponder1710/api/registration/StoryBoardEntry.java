4a40ea54e27ab20ffab94aea85164b86d56182e3
Apostrcfo <temp24919@gmail.com>
2026-06-09 18:19:33 -0300

    correct duplicate lines

--- src/main/java/net/createmod/ponder1710/api/registration/StoryBoardEntry.java
+++ src/main/java/net/createmod/ponder1710/api/registration/StoryBoardEntry.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import java.util.function.Consumer;
 import net.createmod.ponder1710.api.level.PonderLevel;
-import java.util.function.Consumer;
-import net.createmod.ponder1710.api.level.PonderLevel;
 import net.createmod.ponder1710.api.scene.PonderStoryBoard;
 
 // import net.minecraft.resources.ResourceLocation; // 1.7.10 uses different package
@@ -13,9 +11,6 @@ import net.minecraft.util.ResourceLocation;
 
 public interface StoryBoardEntry {
 
-    // Consumer<PonderLevel> that places blocks programmatically (alternative to schematic)
-    @javax.annotation.Nullable
-    Consumer<PonderLevel> getWorldSetup();
 
     // Consumer<PonderLevel> that places blocks programmatically (alternative to schematic)
     @javax.annotation.Nullable
